49f7ab779c63850639a02ea8824dd9a1574fb701
FilipBalaban <100449267+FilipBalaban@users.noreply.github.com>
2022-07-15 15:44:36 +0200

    Display score by Javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+// Home
+let homeScoreEl = document.getElementById("home-score")
+let homePoints = 10
+homeScoreEl.textContent = homePoints
+
+// Guest
+let guestScoreEl = document.getElementById("guest-score")
+let guestPoints = 5
+guestScoreEl.textContent = guestPoints
+
