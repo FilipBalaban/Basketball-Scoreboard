7f7ded3a1da23b7e36c559c77ba7e18f256cd7f0
FilipBalaban <100449267+FilipBalaban@users.noreply.github.com>
2022-07-15 15:52:11 +0200

    Increment points functions

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 // Home
 let homeScoreEl = document.getElementById("home-score")
-let homePoints = 10
+let homePoints = 0
 homeScoreEl.textContent = homePoints
 
 // Guest
 let guestScoreEl = document.getElementById("guest-score")
-let guestPoints = 5
+let guestPoints = 0
 guestScoreEl.textContent = guestPoints
 
+// Home points
+
+// +1 Pt
+function ptHome1() {
+    homePoints += 1
+    homeScoreEl.textContent = homePoints
+}
+
+// +2 Pt
+function ptHome2() {
+    homePoints += 2
+    homeScoreEl.textContent = homePoints
+}
+
+// +3 Pt
+function ptHome3() {
+    homePoints += 3
+    homeScoreEl.textContent = homePoints
+}
+
+
+// guest points
+
+// +1 Pt
+function ptGuest1() {
+    guestPoints += 1
+    guestScoreEl.textContent = guestPoints
+}
+
+// +2 Pt
+function ptGuest2() {
+    guestPoints += 2
+    guestScoreEl.textContent = guestPoints
+}
+
+// +3 Pt
+function ptGuest3() {
+    guestPoints += 3
+    guestScoreEl.textContent = guestPoints
+}
